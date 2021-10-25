80b029663f8cd0961e418f15292208ddbe11528a
VladCP <vlad_prisacariu@yahoo.com>
2021-10-25 16:56:20 +0100

    Finished toFoundations
    - not very efficient I think ?

--- Solitaire.hs
+++ Solitaire.hs
@@ -9,7 +9,7 @@ last modified: 25/10/2021
 module Solitaire where
     import Data.List
     import System.Random
-
+    import Debug.Trace
     -- datatypes - basics
     data Suit = Hearts | Clubs | Spades | Diamonds deriving (Show, Eq, Enum)
     data Pip = Two | Three | Four | Five | Six | Seven
@@ -74,12 +74,16 @@ module Solitaire where
                      showOnNewLine d = (show d) ++ "\n"
 
     initialLayout :: Board 
-    initialLayout = EOBoard [(Two,Clubs), (Two, Spades)] [[(Seven,Diamonds),(Ace,Hearts),(Queen,Hearts),(King,Clubs),(Four,Spades)],
-                    [(Five,Diamonds),(Queen,Spades),(Three,Diamonds),(Five,Spades),(Six,Spades),(Seven,Hearts)],
-                    [(King,Hearts),(Ten,Diamonds),(Seven,Spades),(Queen,Diamonds),(Five,Hearts),(Eight,Diamonds)]] [(Two,Hearts),(Six,Clubs),(Five,Clubs),(Jack,Diamonds)]
-
-    testFunc :: [Card] -> Int
-    testFunc c = length(pack)
+    initialLayout = EOBoard []
+                    [[(Six,Clubs),(Seven,Diamonds),(Ace,Hearts),(Queen,Hearts),(King,Clubs),(Four,Spades)],
+                    [(Five,Diamonds), (Queen,Clubs),(Three,Diamonds),(Five,Spades),(Six,Spades),(Seven,Hearts)],
+                    [(King,Hearts),(Ten,Diamonds),(Seven,Spades),(Queen,Diamonds),(Five,Hearts),(Eight,Diamonds)],
+                    [(Jack,Spades),(Six,Hearts),(Seven,Clubs),(Eight,Spades),(Ten,Clubs),(Queen,Clubs)],
+                    [(Ace,Spades),(Eight,Clubs),(Ace,Diamonds),(King,Diamonds),(Jack,Hearts),(Four,Clubs)],
+                    [(Two,Diamonds),(Three,Hearts),(Two,Hearts),(Ten,Hearts),(Six,Diamonds),(Jack,Clubs)],
+                    [(Nine,Spades),(Four,Diamonds),(Nine,Clubs),(Nine,Hearts),(Three,Spades),(Ten,Spades)],
+                    [(Two,Clubs),(Two,Spades),(Four,Hearts),(Nine,Diamonds),(King,Spades),(Eight,Hearts)]
+                    ] [(Three,Clubs),(Ace,Clubs),(Five,Clubs),(Jack,Diamonds)]
 
     --eODeal takes a seed as a paramater
     -- and deals a randomly shuffled deck for eight off 
@@ -95,5 +99,42 @@ module Solitaire where
                  [(take 8 deck)] ++ splitIntoColumns (drop 8 deck)
     
     --TODO
-    toFoundations :: Board -> Board
+    toFoundations :: Board -> Board 
+    toFoundations (EOBoard f c r) = toFoundationsColumns [] (EOBoard f c r)
+
+    --retry to place reserves to foundations every time we place a card from columns to foundations
+    toFoundationsColumns :: Columns -> Board -> Board
+    toFoundationsColumns aux (EOBoard f [] r) = EOBoard f aux r
+    toFoundationsColumns aux (EOBoard f (c:cols) r) 
+         | canBePlaced top f = toFoundationsColumns [] (EOBoard f' (aux ++ ((tail c):cols)) r')
+         | otherwise = toFoundationsColumns (aux ++ [c]) (EOBoard f cols r)
+         where
+             top = head c
+             (r',f') = toFoundationsReserve [] r (placeOnFoundation top f)
+
+
+    toFoundationsReserve :: Reserve -> Reserve -> Foundations -> (Reserve,Foundations)
+    toFoundationsReserve [] [] f = ([], f)
+    toFoundationsReserve aux [] f = (aux, f)
+    toFoundationsReserve aux (r:rs) f 
+                |  canBePlaced r f = toFoundationsReserve [] (aux++rs) (placeOnFoundation r f)
+                |  otherwise = toFoundationsReserve (aux ++ [r]) rs f
     
+    --can a card be placed into the foundations
+    canBePlaced :: Card -> Foundations -> Bool
+    canBePlaced c [] 
+                | isAce c = True
+                | otherwise = False
+    canBePlaced c (c':fnd)
+                | isAce c = True
+                | c == sCard c' = True
+                | otherwise = canBePlaced c fnd
+
+    placeOnFoundation :: Card -> Foundations -> Foundations
+    placeOnFoundation c []
+                        | isAce c = [c]
+                        | otherwise = []
+    placeOnFoundation c f@(c':fnd) 
+                        | isAce c = c:f
+                        | c == sCard c' = c:fnd
+                        | otherwise = c':(placeOnFoundation c fnd)
